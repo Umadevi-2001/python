18be98537c3485f18e1fe1331eeee86b59fde920
Uma Devi Chapala <umadevichapala01@gmail.com>
2026-03-05 10:43:19 +0530

    Update star pattren.py

--- star pattren.py
+++ star pattren.py
@@ -1,35 +1,42 @@
-'''n=int(input())
-for i in range(n):
-    for j in range(i+1):
-        print("*",end=" ")
-    print()'''
-'''* 
-* * 
-* * * 
-* * * * 
-* * * * * '''
-'''
-print()
-print()
-for i in range(n):
-    for j in range(n-i):
-        print("*",end=" ")
-    print()  
-''''''
-print()
-print()
-a=1
-for i in range(n,0,-1):
+"""
+Pattern Programs in Python
+
+This script demonstrates different star and number patterns
+using nested loops in Python.
+"""
+
+# Input from user
+n = int(input("Enter the number of rows: "))
+
+print("\n1. Increasing Star Pattern")
+for i in range(1, n + 1):
     for j in range(i):
-        print(a,end="   ")
-        a+=1
+        print("*", end=" ")
     print()
-'''
-n=int(input())
-a=1
-for i in range(n):
+
+
+print("\n2. Decreasing Star Pattern")
+for i in range(n, 0, -1):
     for j in range(i):
-        print(a,end=" ")
-        a+=1
+        print("*", end=" ")
     print()
+
+
+print("\n3. Continuous Number Triangle Pattern")
+num = 1
+for i in range(n, 0, -1):
+    for j in range(i):
+        print(num, end=" ")
+        num += 1
+    print()
+
+
+print("\n4. Increasing Number Pattern")
+num = 1
+for i in range(1, n + 1):
+    for j in range(i):
+        print(num, end=" ")
+        num += 1
+    print()
+
 
