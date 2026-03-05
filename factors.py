02802546bde35ed626b1a238ce4b7942b1005c4b
Uma Devi Chapala <umadevichapala01@gmail.com>
2026-03-05 10:39:39 +0530

    Update factors.py

--- factors.py
+++ factors.py
@@ -1,3 +1,4 @@
+#check whether num1 is a factor of num2
 num1=int(input())
 num2=int(input())
 if num1!=0 and num2%num1==0:
@@ -5,12 +6,12 @@ if num1!=0 and num2%num1==0:
 else:
     print("not a factor")
 
-num1=int(input())
-num2=int(input())
-if num1==0:
-    print("cannot divide a number with zero")
-elif num2%num1==0:
-    print("not a factor")
-else:
-    print("not a factor")
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
 
+if num1 == 0:
+    print("Cannot divide by zero")
+elif num2 % num1 == 0:
+    print("num1 is a factor of num2")
+else:
+    print("num1 is not a factor of num2")
